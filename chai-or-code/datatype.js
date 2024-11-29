3f17fd9a2a3f03c4ac3c2759e1857611d5bd73e9
Preranabarat <preranabarat@2004>
2024-11-29 20:49:33 +0530

    update

--- chai-or-code/datatype.js
+++ chai-or-code/datatype.js
@@ -2,3 +2,21 @@
 
 // alert(3+3)// we are using nodejs not browser
 console.log(3+3)
+
+console.log("prerana")
+
+
+let name ="prerana"
+let age= 18
+let isLoggedIn=false
+//number=>2 to power 53
+//bigint 
+// string=>"njjh"
+//boolean=>true/false
+//null=>sandalone valu//object
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof age);//number
+
+
