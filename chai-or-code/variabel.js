48ac1266a3fea8e40885e74f14f294a47e56f36a
Preranabarat <preranabarat@2004>
2024-11-29 10:57:11 +0530

    update

---
+++ chai-or-code/variabel.js
@@ -0,0 +1,11 @@
+const accountId = 123456789
+let accountEmail = "prerana334@email.com"
+var accountPassword = "22113344"
+accountCity ="jaipur"
+
+accountEmail="pb@googel.com"
+acccountPassword="34413"
+accountCity="bangaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
