a071e51a2700ae2ec4424be6590f0f8022d1e98a
Preranabarat <preranabarat@2004>
2025-01-22 18:53:31 +0530

    variable

---
+++ 01_basics/01.variables.js
@@ -0,0 +1,10 @@
+const accountId = 14326778
+let accountEmail = "prerana345@gmail.com"
+var accountPassword = "12345"
+console.log(accountId);
+
+//let accountCity;
+accountEmail = "prerana@gmail.com"
+accountPassword = "6537865"
+accountCity = "khatra"
+console.table([accountCity,accountEmail,accountId,accountPassword]);
