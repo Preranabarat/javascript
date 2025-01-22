71e67487b72346a3de26b6b53bd88959419d9f5f
Preranabarat <preranabarat@2004>
2025-01-22 19:52:16 +0530

    convesionoperation

---
+++ 03.conversionOperation.js
@@ -0,0 +1,52 @@
+let score ="33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33"=>33
+//"33abc"=>NaN
+//true=>1 ; false=> 0
+
+let isLoggedIn = "pb"
+let booleanIsLoggdIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggdIn);
+
+//1=>true; 0=>false;
+//""=> false;
+//"pb"=> true;
+
+let someNumber = 33
+let stringNumber= String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//operation
+ 
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2*2);
+// console.log(2**3);//power
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2%3);
+// console.log(2/3);
+
+let str1="hello"
+let str2="Avradip"
+let str3= str1 + " " +str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1 +"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
