d5c9490ed55e391d8027013d89df11fbc06cd072
Preranabarat <preranabarat@2004>
2025-01-22 19:11:55 +0530

    datatype

---
+++ 01_basics/02.datatypes.js
@@ -0,0 +1,21 @@
+// alert("hello")
+let name="prerana"
+let age = 23
+let isLoggedIn =false
+
+
+//number => 2 to power 53
+//bigint 
+//string => " "
+// boolean => true/false
+//null=> standalone value
+//undefined
+//symbpl => unique
+
+//object
+console.log(typeof "");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
