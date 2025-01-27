943ce6dfe8353077fe6318d7cf0f95e8b6114ad9
Preranabarat <preranabarat@2004>
2025-01-27 20:01:07 +0530

    number and math

---
+++ 01_basics/06_number_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 124.2332
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100754000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(43.3));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,2,6,8));
+console.log(Math.max(4,3,2,1,6));
+console.log(Math.random());
+console.log((Math.random()*100)+1);
+console.log(Math.floor(Math.random()*100)+1);
+
+
+const min =10
+const max =20
+console.log(Math.floor(Math.random() * (max-min+1))+min);
