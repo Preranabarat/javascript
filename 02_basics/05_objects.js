917e359b46659271c0ca382a47dc6798b1663d90
Preranabarat <preranabarat@2004>
2025-03-01 10:33:05 +0530

    objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,18 @@
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"prerana"
+}
+
+// course.courseInstructor
+const {courseInstructor:instructor}=course
+console.log(courseInstructor);
+console.log(instructor);
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company ="prerana")
+
+
