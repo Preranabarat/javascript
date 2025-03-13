b72433f0b163687e9c90f7cca17fb7f1c5bff222
Preranabarat <preranabarat@2004>
2025-03-13 20:31:28 +0530

    loop content

---
+++ 05-iterations/two.js
@@ -0,0 +1,24 @@
+// while
+
+let i =0
+while (i<=10) {
+    console.log(`value of index is ${i}`);
+    i = i + 2
+    
+}
+
+let myArray = [ "flash","batmen","superman"]
+let arr=0
+while(arr <myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr++
+}
+
+// do while
+
+let Score =1
+do {
+    console.log(`Score is ${Score}`);
+    Score++
+    
+} while (Score<=10);
